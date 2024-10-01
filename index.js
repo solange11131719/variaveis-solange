5f55e93e4dcdeebe776cfc2b506745c5ddc8b943
solange11131719 <167382822+solange11131719@users.noreply.github.com>
2024-10-01 23:03:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const nomeCompleto = "Solange Sales Generoso Dos Santos";
+const anoNascimento = 1976;
+let anoAtual = 2024;
+let idade = 48;
+
+console.log("Olá eu sou " + nomeCompleto + " estamos no ano " + anoAtual + " minha idade é " + idade + " e sou nascida no ano de " + anoNascimento + ".");
+
+anoAtual = anoAtual + 1;
+idade = idade + 1;
+console.log("Olá meu nome ainda é " + nomeCompleto + "," + " estamos no ano de " + anoAtual + " e finalmente fiz " + idade + " anos, " + " como eu sou nascida em 1976 ano que vem faço 48 anos e as minhas responsabilidades aumentam.");
+
+console.log(`oi eu sou ${nomeCompleto} meu ano de nascimento é ${anoNascimento} meu ano atual é ${anoAtual} e minha idade é ${idade}`);
+
+const listaDeViagens = new Array(
+`Mexico` ,
+`Canada`,
+`Estados Unidos`,
+`Alemanha`,
+`Italia`,
+ cont logincerto =" solange;
+  const senhaCerta = 1234;"
+console.log(" ");
+console.log('AGẼNCIAS DE VIAGENS');
+console.log(" ");
+
+console.log(" faça seu login");
+
+var login = entrada(" Digite seu login");
+var Senha = entrada(" Digite sua senha");
+
+while(login!= ){
+  console.log(" login Incorreto');
+   login = entrada(" Digite seu login novamente");
+  while("senha = senhaCerta");
+                                                     
+              
+}
+console.log(listaDeViagens);
+
+console.log(listaDeViagens[4]);
+
+listaDeViagens.push("Coronel Vivida");
+console.log(listaDeViagens);
+
+listaDeViagens.splice(1,1);
+console.log(listaDeViagens);
+
+const idadeComprador = 24;
+
+if(idadeComprador <18){
+  console.log("Infelizmente, nao podemos vender as passagens para menor de idade");
+}
+if(idadeComprador >= 18){
+  console.log("Parabens! Voce adquiriu sua passagem com sucesso.");
+}
